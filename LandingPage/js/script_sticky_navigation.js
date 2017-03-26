7bb40f7507079893dab4c1e6814d5222e4945753
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-03-26 15:47:00 +0200

    strona portfolio

---
+++ LandingPage/js/script_sticky_navigation.js
@@ -0,0 +1,28 @@
+$(function() {
+	var NavY = $('nav').offset().top;
+	 
+	function stickyNav(){
+		var ScrollY = $(window).scrollTop();
+		  
+		if (ScrollY > NavY) { 
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky'); 
+		}
+	};
+	 
+	stickyNav();
+	 
+	$(window).scroll(function() {
+		stickyNav();
+	});
+});
+  /*$(window).on('scroll', function() {
+    var navHeight = $(window).height() - 50;
+    if  ($(window).scrollTop() > navHeight ) {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  });*/
+
