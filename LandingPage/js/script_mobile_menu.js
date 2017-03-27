6a9bb23f425e47d0131e5cd1a973fc0adaa8b36a
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-03-28 00:19:29 +0200

    mobile

--- LandingPage/js/script_mobile_menu.js
+++ LandingPage/js/script_mobile_menu.js
@@ -1,13 +1,13 @@
 $(function(){
 	$('.hamburger-menu').click(function(){
 		$('li').slideDown();
-		$('.hamburger-menu').fadeOut();
-		$('.icon-close').fadeIn();
+		//$('.hamburger-menu').fadeOut();
+		//$('.icon-close').fadeIn();
 	});
 
 	$('.icon-close').click(function() {
 	    $('li').slideUp();
-	    $('.hamburger-menu').fadeIn();
-	    $('.icon-close').fadeOut();
+	    //$('.hamburger-menu').fadeIn();
+	    //$('.icon-close').fadeOut();
 	});
 });
--- LandingPage/js/script_scroll_to.js
+++ LandingPage/js/script_scroll_to.js
@@ -1,11 +1,10 @@
 $(function() { 
- 
 	$('a[href^="#"]').click(function(event) {
 		event.preventDefault();
 		var target = $($(this).attr('href'));
-		$('li').slideUp(1000);
-		$('.hamburger-menu').fadeIn();
-		$('.icon-close').fadeOut();
+		//$('li').slideUp(1000);
+		//$('.hamburger-menu').fadeIn();
+		//$('.icon-close').fadeOut();
 		if(target.length ){
 			$('html, body').animate({
 				scrollTop: target.offset().top -50
