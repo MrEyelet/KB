b420a31f0696aafb88b0938928e43726809b94d8
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-03-28 01:01:46 +0200

    mobile

--- LandingPage/js/script_scroll_to.js
+++ LandingPage/js/script_scroll_to.js
@@ -2,13 +2,16 @@ $(function() {
 	$('a[href^="#"]').click(function(event) {
 		event.preventDefault();
 		var target = $($(this).attr('href'));
-		//$('li').slideUp(1000);
-		//$('.hamburger-menu').fadeIn();
-		//$('.icon-close').fadeOut();
+		
 		if(target.length ){
 			$('html, body').animate({
 				scrollTop: target.offset().top -50
 			}, 1000);
+		}
+		if($(document).width() <= 767) {
+			$('li').slideUp(1000);
+			$('.hamburger-menu').fadeIn();
+			$('.icon-close').hide();
 		}
 	});
 });
